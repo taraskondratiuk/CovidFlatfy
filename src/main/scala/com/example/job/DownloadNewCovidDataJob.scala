c62c9a2398f0f218c00eaf793f2bccdece03b640
Taras <kondratiuk.taras123@gmail.com>
2020-06-11 22:06:35 +0300

    job fix

--- src/main/scala/com/example/job/DownloadNewCovidDataJob.scala
+++ src/main/scala/com/example/job/DownloadNewCovidDataJob.scala
@@ -3,10 +3,9 @@ package com.example.job
 import com.example.utility.FileDownloader
 import org.quartz.{Job, JobExecutionContext}
 
-class DownloadNewCovidDataJob extends Job {
+class DownloadNewCovidDataJob extends Job with FileDownloader {
   override def execute(context: JobExecutionContext): Unit = {
-    val fileDownloader = FileDownloader()
     val jobDataMap = context.getMergedJobDataMap
-    fileDownloader.downloadFile(jobDataMap.getString("url"), jobDataMap.getString("path"))
+    downloadFile(jobDataMap.getString("url"), jobDataMap.getString("path"))
   }
 }
